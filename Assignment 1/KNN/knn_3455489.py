cb5fdacc54afcd8ca55d028f184d453917c102b2
Quoc Le <quoc125@Knights.ucf.edu>
2019-09-24 22:32:48 -0400

    Final submision

--- Assignment 1/KNN/knn_3455489.py
+++ Assignment 1/KNN/knn_3455489.py
@@ -5,31 +5,84 @@
 import argparse
 import numpy as np
 
+#class to store Iris infomation
 class Iris:
   def __init__(self, sepal_length, sepal_width,
               petal_length, petal_width, type):
-      self.sepal_length = sepal_length
-      self.sepal_width = sepal_width
-      self.petal_length = petal_length
-      self.petal_width = petal_width
+      self.sepal_length = float(sepal_length)
+      self.sepal_width = float(sepal_width)
+      self.petal_length = float(petal_length)
+      self.petal_width = float(petal_width)
       self.type = type
+      self.dataList = np.zeros(4, dtype=float)
+      self.dataList[0] =  self.sepal_length
+      self.dataList[1] =  self.sepal_width
+      self.dataList[2] =  self.petal_length
+      self.dataList[3] =  self.petal_width
+
 
 #Return the cosine difference between two vector
 def cosDif(v1,v2):
     numerator = np.dot(v1,v2)
-    denominator = np.absolute(v1) * np.absolute(v1)
-    return 1-np.arccos(numerator/denominator)
+    denominator = absolute(v1) * absolute(v2)
+    return 1-numerator/denominator
+
+#Give the absolute value of the vector
+def absolute(v1):
+    sum = 0
+    for i in range(4):
+        sum = sum + np.square(v1[i])
+    return np.sqrt(sum)
 
 #Return the euclidean difference between two vector
 def Euclidean(v1, v2):
     sum = 0
-    for x, y in v1, v2:
-        sum  = sum + np.square(y-x)
+    for i in range(4):
+        sum  = sum + np.square(v1[i]-v2[i])
     return np.sqrt(sum)
 
-def findNearNeighbor(training, test, k, method):
+def Predict(voter):
+    total = 0
+    convert = {"Iris-setosa":0,  "Iris-versicolor":1, "Iris-virginica":2}
+    matrix = [[0,0,0],[0,0,0],[0,0,0]]
+    for input in voter:
+        count = {}
+        for i in range(len(voter[input])):
+            if voter[input][i][1] not in count:
+                count[voter[input][i][1]] = 0
+            count[voter[input][i][1]] =  count[voter[input][i][1]] +1
+        winner = None
+        TopCount = 0
+        for type in count:
+            if TopCount < count[type]:
+                winner = type
+                TopCount = count[type]
+        matrix[convert[winner]][convert[input.type]] = matrix[convert[winner]][convert[input.type]] + 1
+        if winner == input.type:
+            total = total +1
+    accuracy = float(total)/ len(voter)    
+    return accuracy
 
-    return false
+def findNearNeighbor(training, test, k, method):
+    voter = {}
+    for y in test:
+        voter[y] = []
+        for i in range(k):
+            voter[y].append( [99999, None])
+        for val in training:
+            distance = 0
+          
+            if method == "Euclidean":
+                distance = distance + Euclidean(val.dataList, y.dataList)
+            elif method == "Cosine":
+                distance = distance + cosDif(val.dataList, y.dataList)
+
+            for i in range(k):
+                if float(voter[y][i][0]) > distance:
+                    voter[y][i][0] = distance
+                    voter[y][i][1] = val.type
+                    break;       
+    return voter
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
@@ -47,14 +100,20 @@ if __name__ == "__main__":
            line = fp.readline()
     
     i = 0
+    j = 0
     testSet = []
     trainingSet = []
+    crossValidation = []
     size = int(len(List)/5)
-
+    accuracy = 0
     while (i < len(List)):
         trainingSet = (List[0:i])
         trainingSet.extend(List[i+size: len(List)])
         testSet = List[i : i+size]
+        voter = findNearNeighbor(trainingSet,testSet,int(args["k"]), args["method"])
+        tempAccuracy= Predict(voter)
+        accuracy = accuracy + tempAccuracy/5
 
+        j = j +1
         i = i + size
-
+    print(args["k"] + " nearest neighbors using " +args["method"] + " difference with an accuracy of " +str(accuracy * 100) + "%")
--- Assignment 1/Naive Baye/bayes_3455489.py
+++ Assignment 1/Naive Baye/bayes_3455489.py
@@ -1,18 +1,119 @@
 #Quoc Le
 #3455489
 #Naive Bayes
+import numpy as np
+
+class Iris:
+    def __init__(self, sepal_length, sepal_width,
+              petal_length, petal_width, type):
+      self.sepal_length = sepal_length
+      self.sepal_width = sepal_width
+      self.petal_length = petal_length
+      self.petal_width = petal_width
+      self.type = type
+      self.dataList = [sepal_length,sepal_width, petal_length, petal_width]
+
+#Split training set for each class
+def splitData(data):
+    newData = {}
+    for x in data:
+        if (x.type not in newData):
+            newData[x.type] = []       
+        newData[x.type].append(x.dataList)
+    return newData
+
+#Calculate the mean  and return count and mean
+def calMean(data):
+    means = {}
+    for y in data:
+        if y not in means:
+            means[y] = [0,0,0,0]
+        for i in range(len(data[y])):
+            for j in range(4):
+                means[y][j] = means[y][j] +float(data[y][i][j])/len(data[y])    
+    return means
+
+#Calcuate the standard deviation 
+def calStdDev(mean,data):
+    stdDev = {}
+    for y in data:
+        if y not in stdDev:
+            stdDev[y] = [0,0,0,0]
+        for i in range(len(data[y])):
+            for j in range(4):
+               stdDev[y][j] = stdDev[y][j] + (np.square(mean[y][j] - float(data[y][i][j])))/(len(data[y])-1)
+        for i in range(4):
+            stdDev[y][i] = np.sqrt(stdDev[y][i])
+    return stdDev
+
+#Calculate the probablity 
+def calProb(x, mean, stdDev, training):
+    classProp = {}
+    for y in mean:
+        if y not in classProp:
+            classProp[y] = [0,0,0,0]
+        for i in range(4):          
+            classProp[y][i] = np.exp(-1 * (np.square(float(x.dataList[i]) - mean[y][i]))/(2*np.square(stdDev[y][i])))/(np.sqrt(2 *np.pi)*stdDev[y][i])
+    prop = {}
+    for y in classProp:
+        if y not in prop:
+            prop[y] = len(training[y])/130 
+        for i in range(4):
+            prop[y] = prop[y] * classProp[y][i]
+    normalize = 0
+    for y in prop:
+        normalize = normalize+ prop[y]
+    for y in prop:
+        prop[y] = prop[y]/normalize
+    return prop
+
+#Make prediction
+def Predict(prop):
+    bestLabel = None
+    bestValue = 0
+    for y in prop:
+        if bestValue < prop[y]:
+            bestLabel = y
+            bestValue = prop[y]
+    return bestLabel
 
 if __name__ == "__main__":
     List = []
     with open('iris.data') as fp:
        line = fp.readline()
        while line:
-           parameter = line.strip().split(',')
+           parameter = line.strip().split(',')        
            if (len(parameter) == 5):
                 List.append(Iris(parameter[0],parameter[1],parameter[2], parameter[3], parameter[4]))
            line = fp.readline()
     
+
     i = 0
+    size = int(len(List)/5)
+
     testSet = []
     trainingSet = []
-    size = int(len(List)/5)
+    convert = {"Iris-setosa":0,  "Iris-versicolor":1, "Iris-virginica":2}
+    accuracy = 0
+
+    while i < len(List):
+        trainingSet = (List[0:i])
+        trainingSet.extend(List[i+size: len(List)])
+        testSet = List[i : i+size]
+        matrix = [[0,0,0],[0,0,0],[0,0,0]]
+        count = 0
+
+        splitTrainingData = splitData(trainingSet)
+        mean = calMean(splitTrainingData)
+        stdDev = calStdDev(mean,splitTrainingData)
+        for data in testSet:        
+            prop = calProb(data, mean, stdDev, splitTrainingData)
+            prediction = Predict(prop)
+            if prediction == data.type:
+                count  = count +1
+            matrix[convert[prediction]][convert[data.type]] = matrix[convert[prediction]][convert[data.type]] +1
+        accuracy = accuracy +(count/len(testSet))/5
+        i = i + size
+
+    print("Naive Bayes had an accuracy of " + str(accuracy*100) + "%")
+
